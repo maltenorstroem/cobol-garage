41090298c192bf3e1e36df6779b61761c9556182
malte.norstroem <malte.norstroem@gmail.com>
2021-12-13 11:51:53 +0100

    feat: user prompt

--- hello.cbl
+++ hello.cbl
@@ -2,13 +2,19 @@
       / Author: Malte Norstroem
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
+       AUTHOR. MALTENORSTROEM.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
       *INDIVIDUAL VARIABLES
        01 GREETING PIC A(32).
+       01 USER-MSG PIC X(32).
        PROCEDURE DIVISION.
            MOVE 'Hello World' TO GREETING.
            DISPLAY GREETING.
            DISPLAY 'Malte is back in town'.
-           GOBACK.
+       DISPLAY-ACCEPTED-MESSAGE.
+           DISPLAY 'ENTER TEXT:'.
+           ACCEPT USER-MSG.
+           DISPLAY USER-MSG.
+           STOP RUN.
