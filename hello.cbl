e0e6d8dbc8d844980498fc23795081489bee4d04
malte.norstroem <malte.norstroem@gmail.com>
2021-12-17 09:11:21 +0100

    chore: better readable code

--- hello.cbl
+++ hello.cbl
@@ -1,20 +1,42 @@
       / line comment on column 7
       / Author: Malte Norstroem
+      *------------------------
        IDENTIFICATION DIVISION.
+      *------------------------
        PROGRAM-ID. HELLO.
        AUTHOR. MALTENORSTROEM.
+      *-------------
        DATA DIVISION.
+      *-------------
        FILE SECTION.
+
        WORKING-STORAGE SECTION.
       *INDIVIDUAL VARIABLES
-       01 GREETING PIC A(32).
-       01 USER-MSG PIC X(32).
+       77 GREETING PIC A(32).
+       77 USER-MSG PIC X(32).
+       77 COUNTER PIC 9(3).
+
+      *-------------------
        PROCEDURE DIVISION.
+      *-------------------
            MOVE 'Hello World' TO GREETING.
            DISPLAY GREETING.
            DISPLAY 'Malte is back in town'.
-       DISPLAY-ACCEPTED-MESSAGE.
+
+       1000-LOOP.
+           PERFORM VARYING COUNTER FROM 1 BY 1 UNTIL COUNTER EQUAL 21
+             PERFORM 2000-SHOW
+           END-PERFORM.
+       1000-LOOP-END.
+
+       2000-SHOW.
+           DISPLAY GREETING.
+       2000-SHOW-END.
+
+      *Using intrinsic functions (built-in functions)
+       3000-DISPLAY-ACCEPTED-MESSAGE.
            DISPLAY 'ENTER TEXT:'.
            ACCEPT USER-MSG.
-           DISPLAY USER-MSG.
+           DISPLAY 'YOUR TEXT: ' FUNCTION UPPER-CASE(USER-MSG).
            STOP RUN.
+       3000-DISPLAY-ACCEPTED-MESSAGE-END.
